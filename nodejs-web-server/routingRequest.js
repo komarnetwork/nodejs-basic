10f4c85ded1fb50c045031c6d4fa1197b0b1b962
komarnetwork <65493176+komarnetwork@users.noreply.github.com>
2023-12-24 12:16:03 +0700

    Add Response Status

--- nodejs-web-server/routingRequest.js
+++ nodejs-web-server/routingRequest.js
@@ -4,20 +4,23 @@ const http = require('http');
 const requestListener = (request, response) => {
   response.setHeader('Content-Type', 'text/html');
 
-  response.statusCode = 200;
+  // response.statusCode = 200;
   const { url, method } = request;
 
   // TODO! Latihan Mendapatkan Routing Request
   if (url === '/') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Ini Halaman HomePage</h1>');
     } else {
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`,
       );
     }
   } else if (url === '/about') {
     if (method === 'GET') {
+      response.statusCode = 200;
       response.end('<h1>Halo! Ini adalah halaman about</h1>');
     } else if (method === 'POST') {
       let body = [];
@@ -27,6 +30,7 @@ const requestListener = (request, response) => {
       });
 
       request.on('end', () => {
+        response.statusCode = 200;
         /*global Buffer, Buffer*/
         /*eslint no-undef: "error"*/
         body = Buffer.concat(body).toString();
@@ -35,11 +39,13 @@ const requestListener = (request, response) => {
         response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`,
       );
     }
   } else {
+    response.statusCode = 404;
     response.end('<h1>Halaman tidak ditemukan!</h1>');
   }
 };
