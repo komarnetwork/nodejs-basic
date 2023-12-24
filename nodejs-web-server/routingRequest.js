6038193eacb248557f621dc6bb7de3ffa36697b3
komarnetwork <65493176+komarnetwork@users.noreply.github.com>
2023-12-24 11:20:32 +0700

    Latihan Mendapatkan Routing Request

---
+++ nodejs-web-server/routingRequest.js
@@ -0,0 +1,51 @@
+// Latihan Routing Request
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'text/html');
+
+  response.statusCode = 200;
+  const { url, method } = request;
+
+  // TODO! Latihan Mendapatkan Routing Request
+  if (url === '/') {
+    if (method === 'GET') {
+      response.end('<h1>Ini Halaman HomePage</h1>');
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`,
+      );
+    }
+  } else if (url === '/about') {
+    if (method === 'GET') {
+      response.end('<h1>Halo! Ini adalah halaman about</h1>');
+    }
+
+    if (method === 'POST') {
+      let body = [];
+      request.on('data', (chunk) => {
+        body.push(chunk);
+      });
+
+      request.on('end', () => {
+        /*global Buffer, Buffer*/
+        /*eslint no-undef: "error"*/
+        body = Buffer.concat(body).toString();
+
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Halo, ${name}! Ini adalah halaman about</h1>`);
+      });
+    }
+  } else {
+    response.end('<h1>Halaman tidak ditemukan!</h1>');
+  }
+};
+
+const server = http.createServer(requestListener);
+
+const port = 6000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+  console.log(`Server berjalan pada http://${host}:${port}`);
+});
--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -16,6 +16,38 @@
 //   console.log(`Server berjalan pada http://${host}:${port}`);
 // });
 
+// const http = require('http');
+
+// const requestListener = (request, response) => {
+//   response.setHeader('Content-Type', 'text/html');
+
+//   response.statusCode = 200;
+//   const { method } = request;
+
+//   // TODO! Latihan Handling Request
+//   if (method === 'GET') {
+//     response.end('<h1>Hallo HTTP</h1>');
+//   }
+//   if (method === 'POST') {
+//     response.end('<h1>Hallo HTTP POST</h1>');
+//   }
+//   if (method === 'PUT') {
+//     response.end('<h1>Hallo HTTP PUT</h1>');
+//   }
+//   if (method === 'DELETE') {
+//     response.end('<h1>Hallo HTTP DELETE</h1>');
+//   }
+// };
+
+// const server = http.createServer(requestListener);
+
+// const port = 6000;
+// const host = 'localhost';
+
+// server.listen(port, host, () => {
+//   console.log(`Server berjalan pada http://${host}:${port}`);
+// });
+
 const http = require('http');
 
 const requestListener = (request, response) => {
@@ -24,18 +56,37 @@ const requestListener = (request, response) => {
   response.statusCode = 200;
   const { method } = request;
 
-  // Latihan Handling Request
+  // TODO! Latihan Mendapatkan Body Request
   if (method === 'GET') {
     response.end('<h1>Hallo HTTP</h1>');
   }
   if (method === 'POST') {
-    response.end('<h1>Hallo HTTP POST</h1>');
-  }
-  if (method === 'PUT') {
-    response.end('<h1>Hallo HTTP PUT</h1>');
-  }
-  if (method === 'DELETE') {
-    response.end('<h1>Hallo HTTP DELETE</h1>');
+    // deklarasikan variabel body dan inisialisasikan nilainya dengan array kosong
+    let body = [];
+
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on('end', () => {
+      /** 
+      Menampilkan data body yang bernilai data string JSON
+      Hasil : Hai, {"name": "Dicoding"}! 
+      **/
+
+      /*global Buffer, Buffer*/
+      /*eslint no-undef: "error"*/
+      body = Buffer.concat(body).toString();
+
+      /** 
+      Menampilkan data body dari yang bernilai data string JSON
+      Hasil {"name": "Dicoding"} diconvert dengan menggunakan JSON.parse untuk mengubah
+      JSON string menjadi JavaScript objek
+      Hasil : Hai, Dicoding!
+      **/
+      const { name } = JSON.parse(body);
+      response.end(`<h1>Hai, ${name}!</h1>`);
+    });
   }
 };
 
